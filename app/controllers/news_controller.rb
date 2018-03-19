fa7e5eab09ad02184f81a3e2a064f3af4a15e47e
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-18 21:36:03 -0400

    search for articles

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -6,4 +6,20 @@ class NewsController < ApplicationController
     @image_placeholder = 'https://placehold.it/50x50'
     @articles ||= []
   end
+
+  def search
+    @image_placeholder = 'https://placehold.it/50x50'
+    query = params[:q]
+    @articles = fetch_news_query query unless !query
+    @articles ||= [] # empty array if no query
+  end
+
+  private
+
+  def fetch_news_query query
+    url = "https://newsapi.org/v2/everything?q=#{query}&apiKey=#{ENV['NEWS_API_KEY']}&language=en"
+    response = HTTParty.get url
+    response.parsed_response['articles'] unless response.code != 200
+  end
+
 end
