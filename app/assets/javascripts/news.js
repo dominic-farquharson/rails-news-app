8895fde71588f997f2042324373a8df3194799e7
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-23 19:56:33 -0400

    style articles

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const articles = document.querySelectorAll('.article');
+  const articles = document.querySelectorAll('.article-title');
   articles.forEach(el => el.addEventListener('click', openArticle));
   
   function openArticle(e) {
