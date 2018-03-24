294005d5a6ee6e2232196aac9766828a7753cbe4
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-23 20:51:52 -0400

    update user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  PASSWORD_LENGTH: (5..20)
+  
+  # validations
+  validates_presence_of :username
+  validates :password, length: PASSWORD_LENGTH, allow_nil: false
+
+  attr_reader :password
+
+  # class method
+  def self.find_from_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user if user.is_password?(password)
+  end
+
+  # confirm password
+  def is_password?(password_attempt)
+    BCrypt::Password.new(password_digest).is_password?(password_attempt)
+  end
+
+  # set password digest
+  def password=(raw_password)
+    self.password_digest = BCrypt::Password.create(raw_password)
+  end
+end
