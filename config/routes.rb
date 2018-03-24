9bff798be2f36dbf34567197dc0c4e2b349b7715
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-23 20:52:03 -0400

    add resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'news#index'
   get '/search', to: 'news#search'
+
+  resource :session, only: [:new, :create, :destroy]
 end
