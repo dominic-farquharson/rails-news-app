b4419783e7b7c5cb35b7ed9bda2a871ba0415d88
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-23 21:34:30 -0400

    create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :ensure_signed_out, only: [:new, :create]
+  before_action :ensure_signed_in, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(create_user_params)
+
+    if @user.save
+      sign_in(@user)
+      flash[:notice] = 'Welcome to the NewsApp'
+      redirect_to root
+    else 
+      flash[:error] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def create_user_params
+    params.require(:user).permit(:username, :password, :email, :last_name, :first_name)
+  end
+end
