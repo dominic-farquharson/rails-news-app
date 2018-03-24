cf458b124985fbe0520d7f191f8382f405aa4be9
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-23 21:33:44 -0400

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # make method accessible in view  
+  helper_method :current_user 
+
+  private
+
   # sign in user
   def sign_in(user)
     token = SecureRandom.urlsafe_base64 # create random 22 char string
@@ -26,7 +31,17 @@ class ApplicationController < ActionController::Base
     current_user.update_attribute(:session_token , nil)
   end
 
-  private
+  def ensure_signed_in 
+    return if current_user
+    flash[:error] = 'Please sign in to view this page'
+    redirect_to :root
+  end
+
+  def ensure_signed_out
+    return unless current_user
+    flash[:error] = 'You are already logged in'
+    redirect_to :root
+  end
 
   def fetch_news
     url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ENV['NEWS_API_KEY']}"
